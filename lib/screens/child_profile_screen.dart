fa32e0739660186970fa74043f5c561bd7781392
Farhan Arief Ramadhan <farhannadia.nadia@gmail.com>
2023-03-02 13:04:37 +0700

    ubah child_profile_screen.dart

--- lib/screens/child_profile_screen.dart
+++ lib/screens/child_profile_screen.dart
@@ -8,29 +8,75 @@ class ChildProfileCreationScreen extends StatefulWidget{
 }
 
 class ChildProfileCreationScreenState extends State<ChildProfileCreationScreen>{
+  TextEditingController _nameController = TextEditingController();
+  TextEditingController _ageController = TextEditingController();
+  TextEditingController _weightController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        Text(
-          "Ini screen untuk buat child profile",
-          textAlign: TextAlign.center,
-        ),
-        //TODO: buat profile creation
-        SizedBox(
-          height: 32,
-          child: ElevatedButton(
-            onPressed: (){
-              Navigator.pop(context);
-            },
-            child: Text(
-                "Back"
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Text(
+            "Ini screen untuk buat child profile",
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(
+            width: double.infinity,
+            height: 32,
+            child: TextFormField(
+              controller: _nameController,
+              decoration: const InputDecoration(
+                hintText: "Nama",
+              ),
             ),
           ),
-        ),
-      ],
+          SizedBox(
+            width: double.infinity,
+            height: 32,
+            child: TextFormField(
+              controller: _ageController,
+              decoration: const InputDecoration(
+                hintText: "Usia",
+              ),
+            ),
+          ),
+          SizedBox(
+            width: double.infinity,
+            height: 32,
+            child: TextFormField(
+              controller: _weightController,
+              decoration: const InputDecoration(
+                hintText: "Berat Badan",
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 32,
+            child: ElevatedButton(
+              onPressed: (){
+                //TODO: submit form
+              },
+              child: Text(
+                  "Create Profile"
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 32,
+            child: ElevatedButton(
+              onPressed: (){
+                Navigator.pop(context);
+              },
+              child: Text(
+                  "Back"
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 
